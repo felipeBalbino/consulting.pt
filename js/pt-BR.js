0e501c4a93ea0fd90a7a6696405e5a35025fbe3e
Felipe <phellipe86@gmail.com>
2023-06-29 08:55:44 -0300

    Changes to be committed: 	deleted:    css/mediaelementplayer.css 	modified:   index.html 	deleted:    js/mediaelement-and-player.min.js 	modified:   js/pt-BR.js

--- js/pt-BR.js
+++ js/pt-BR.js
@@ -1,11 +1,18 @@
   
 var translations = {
-  "home_title": "Somos uma agência de consultoria sediada em Lisboa, Portugal.",
-  "home_sub_text": "Bem-vindo à nossa vibrante e dinâmica empresa de consultoria, onde juventude e inteligência se encontram! Como uma consultoria jovem e ágil, trazemos perspectivas frescas e ideias inovadoras para cada projeto que assumimos. Nossa equipe de mentes brilhantes está dedicada a fornecer soluções perspicazes que impulsionam o sucesso e o crescimento. Juntos, navegamos pela paisagem empresarial em constante evolução com criatividade, inteligência e uma paixão pela excelência. Junte-se a nós nessa jornada emocionante enquanto desbloqueamos novas possibilidades e moldamos um futuro mais brilhante juntos.",
-  "menu_home": "Principal",
-  "menu_about": "Sobre",
-  "menu_services": "Serviços",
-  "menu_blog": "Blog",
-  "menu_contact": "Contato",
-  "title_consultation": "Consultation",
-  };
+    "home_title": "Somos uma agência de consultoria sediada em Lisboa, Portugal.",
+    "home_sub_text": "Bem-vindo à nossa vibrante e dinâmica empresa de consultoria, onde juventude e inteligência se encontram! Como uma consultoria jovem e ágil, trazemos perspectivas frescas e ideias inovadoras para cada projeto que assumimos. Nossa equipe de mentes brilhantes está dedicada a fornecer soluções perspicazes que impulsionam o sucesso e o crescimento. Juntos, navegamos pela paisagem empresarial em constante evolução com criatividade, inteligência e uma paixão pela excelência. Junte-se a nós nessa jornada emocionante enquanto desbloqueamos novas possibilidades e moldamos um futuro mais brilhante juntos.",
+    "menu_home": "Principal",
+    "menu_about": "Sobre",
+    "menu_services": "Serviços",
+    "menu_blog": "Blog",
+    "menu_contact": "Contato",
+    "title_consultation": "Consulta",
+    "home_entre_contato":"Entre em contato",
+    "menu_navigation":"Navegação",
+    "label_contact_country":"Portugal",
+    "label_contact_adress":"Av. Vale de Milhaços 129-133 - Seixal - Portugal",
+    "label_contact_email":"hello@mydomain.com",
+    "label_contact_tel":"+1 829 2293 382",
+    "label_footer_rights":"Todos os direitos reservados."
+};
